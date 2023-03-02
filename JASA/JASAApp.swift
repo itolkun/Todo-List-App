eb7e983b56d02e21c7b283a2e87e65e4ca736628
Aitolkun <totita@list.ru>
2023-03-02 12:31:13 +0300

    Initial Commit

---
+++ JASA/JASAApp.swift
@@ -0,0 +1,17 @@
+//
+//  JASAApp.swift
+//  JASA
+//
+//  Created by Айтолкун Анарбекова on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct JASAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
